4f1d7160d6f054d79da7f84b49dde1f67ea99df4
billikw <laokas@protonmail.com>
2022-08-09 09:46:33 +0100

    cleaned up function

--- app.js
+++ app.js
@@ -17,9 +17,5 @@ mobileButton.addEventListener("click", () => {
   toggleClass("mobile");
 });
 toggleClass = (attr) => {
-  card.classList.remove("twoCol");
-  card.classList.remove("twoColOffset");
-  card.classList.remove("gap");
-  card.classList.remove("mobile");
-  if (!card.classList.contains(attr)) card.classList.add(attr);
+  card.className = `card ${attr}`;
 };
